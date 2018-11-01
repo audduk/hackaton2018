a3660855da9b13f4760a71af889604f089e45d8d
Khayrutdinov Marat <mail@wtiger.org>
2018-11-01 23:56:07 +0300

    Обновили правила

--- hackathon.sol
+++ hackathon.sol
@@ -18,7 +18,6 @@ contract FileTrasfer {
         info.exists = true;
         info.sender = msg.sender;
         info.sendTime = now;
-        files[hash] = info;
     }
 
     struct TransferInfo {
@@ -38,6 +37,7 @@ contract FileTrasfer {
     /// Файл (зашифрованный) передан получателю
     /// Вызывает отправитель файла после факта отправки
     function registerResponse(bytes32 hash, bytes32 encryptedHash, bytes32 password) public {
+        log2(hash, encryptedHash, password);
         require(files[hash].exists);
         require(!transfers[encryptedHash].registered);
         TransferInfo storage info = transfers[encryptedHash];
@@ -46,29 +46,29 @@ contract FileTrasfer {
         info.encryptedHash = encryptedHash;
         info.password = password;
         info.transferTime = now;
-        transfers[encryptedHash] = info;
     }
 
     /// Запрос пароля, выданного для заданного зашифрованного файла
     /// Вызывает получатель файла после расчета хеша полученного зашифрованного файла
-    function approveReceiving(bytes32 encryptedHash) public returns (bytes32) {
-        require(transfers[encryptedHash].registered);
-        require(!transfers[encryptedHash].received);
+    function approveReceiving(bytes32 encryptedHash) public {
         TransferInfo storage info = transfers[encryptedHash];
+        log1(encryptedHash, info.encryptedHash);
+        require(info.registered);
+        require(!info.received);
+        require(info.encryptedHash == encryptedHash);
         info.received = true;
         info.receiver = msg.sender;
         info.receiveTime = now;
-        transfers[encryptedHash] = info;
-        return info.password;
     }
 
     function ping(uint p1, uint p2) public pure returns (uint) {
         return p1 + p2;
     }
 
-    function getResponse(bytes32 encryptedHash) public view returns (bytes32, address, bytes32) {
+    function getResponse(bytes32 encryptedHash) public returns (bytes32, address, bytes32) {
         require(transfers[encryptedHash].received);
-        TransferInfo storage info = transfers[encryptedHash];
+        TransferInfo memory info = transfers[encryptedHash];
+        log1(info.hash, info.password);
         return (info.hash, info.receiver, info.password);
     }
 }
